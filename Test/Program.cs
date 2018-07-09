59ce3670a7800f6ab469ee84993220143963d9c5
sdnuliulei <sdnuliulei@gmail.com>
2018-07-09 19:35:22 +0800

    test

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey();
         }
     }
 }
